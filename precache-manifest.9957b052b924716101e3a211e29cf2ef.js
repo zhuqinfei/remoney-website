1f131e8d378a69abc071f29dbb7e0efdbe75a431
zhuqinfei <1586859762@qq.com>
2022-06-09 19:42:33 +0800

    deploy

---
+++ precache-manifest.9957b052b924716101e3a211e29cf2ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1301aa7b4d6c944d73725940cc22bd0",
+    "url": "/remoney-website/index.html"
+  },
+  {
+    "revision": "5c5791416eaa4332c29e0db8fc7e1b3d",
+    "url": "/remoney-website/manifest.json"
+  },
+  {
+    "revision": "2fdc4258f705d4239988",
+    "url": "/remoney-website/public/css/app.2013cba4.css"
+  },
+  {
+    "revision": "8bd77bbe0938a8c13f63",
+    "url": "/remoney-website/public/css/chunk-vendors.cc4981e2.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/remoney-website/public/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/remoney-website/public/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "2fdc4258f705d4239988",
+    "url": "/remoney-website/public/js/app.9a1e6084.js"
+  },
+  {
+    "revision": "8bd77bbe0938a8c13f63",
+    "url": "/remoney-website/public/js/chunk-vendors.b426bcdc.js"
+  },
+  {
+    "revision": "24db9972ef3480471bf0cf9517452426",
+    "url": "/remoney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/remoney-website/robots.txt"
+  }
+]);
